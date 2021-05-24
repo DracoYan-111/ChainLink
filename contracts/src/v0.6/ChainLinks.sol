79f6e00bd9fc21a8710f062f48c9236c69c4828a
29449 <2944972991@qq.com>
2021-05-24 16:18:10 +0800

    删除非必要代码

--- contracts/src/v0.6/ChainLinks.sol
+++ contracts/src/v0.6/ChainLinks.sol
@@ -1,4 +1,5 @@
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 interface AggregatorInterface {
     function latestAnswer() external view returns (int256);
     function latestTimestamp() external view returns (uint256);
